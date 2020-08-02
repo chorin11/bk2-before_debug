c7f565c4611f5e7a00354aabf2ebb302bff4dc94
きむらあやか <ha.iy.r8ao@gmail.comgit config --global user.name chorin11>
2020-08-02 16:48:02 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @books = Book.new
+    @book = Book.find(params[:id])
+    @users = User.all
+    @user = @book.user
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @user = current_user
+    @book.user = @user
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+      redirect_to books_path
+    else
+      render "edit"
+    end
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      flash[:error] = "error"
+      render "edit"
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+    @book.destoy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'home#top'
+  get 'home/about' => 'home#about'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :create, :update, :show, :edit]
+  resources :books
+end
